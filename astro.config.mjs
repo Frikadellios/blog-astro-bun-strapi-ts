61e30aa934d1886e4c377a654ea17402f6fc161f
Hrihorii Ilin <devopsick@pm.me>
2023-09-15 11:27:48 +0000

    + Strapi

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,23 @@ import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind(), partytown()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    react(), 
+    tailwind({
+    configFile: './tailwind.config.cjs',
+  }), 
+  partytown(), 
+  solidJs()
+],
   output: "server",
   adapter: node({
     mode: "standalone"
