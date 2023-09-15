853b61c856b8bf6982bae2d6d8ed53092915a9dd
Hrihorii Ilin <devopsick@pm.me>
2023-09-15 10:11:19 +0000

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), tailwind(), partytown()],
   output: "server",
   adapter: node({
     mode: "standalone"
